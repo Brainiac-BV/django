75ae33fc6d5e829edc74884f56fb7f0f0f3c4f34
brainiac-bv <k.dgray23@gmail.com>
2019-02-18 19:11:34 +0000

    weekend work

--- backend/src/djreactp/articles/api/views.py
+++ backend/src/djreactp/articles/api/views.py
@@ -1,8 +1,18 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-from  articles.models import Article
+from articles.models import Article
 from .serializers import ArticleSerializer
 
+'''
+Each class below return json represenation of model to view
+'''
 
-class ArticleListView():
-    pass
+
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
